480b057c484e845adb13b146b391b60bd4fd20e7
sxrxhmxsxrxs <sarahmisaras@icloud.com>
2023-10-22 21:41:09 +0200

    fix: start -> playerNames -> initPokemons -> game routes run smoothly now

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,9 +4,7 @@ import de.htwg.se.pokelite.controller.impl.Controller
 import de.htwg.se.pokelite.model.states.{DesicionState, FightingState, GameOverState, InitPlayerPokemonState, InitPlayerState, InitState, SwitchPokemonState}
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
-import play.twirl.api.Html
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -15,124 +13,95 @@ import play.twirl.api.Html
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-    val controller = new Controller();
+    val controller: Controller = Controller()
 
-  def rules = Action {
-    Ok(views.html.rules())
-  }
-
-  def playerNames = Action {
-    Ok(views.html.playerNames())
-  }
- //addplayers route could also be initplayersroute!
-  /*def addPlayers = Action { implicit request =>
-    val player1Name = request.body.asFormUrlEncoded.get("player1Name").mkString
-    val player2Name = request.body.asFormUrlEncoded.get("player2Name").mkString
-    val game = gameController.game
-
-    /*game.state.initPlayers()
-    println("0 state " + game.state)
-    game.state.addPlayer(player1Name)
-    game.state.addPlayer(player2Name)*/
-
-    gameController.initPlayers()
-    gameController.addPlayer(player1Name)
-    gameController.addPlayer(player2Name)
-
-
-    println("1 state " + gameController.game.state)
-
-
-    val gameString = gameController.game.toString;
-    val gameField = views.html.game(gameString)
-
-    Redirect(routes.HomeController.game).flashing(
-      "player1Name" -> player1Name,
-      "player2Name" -> player2Name,
-      "gameState" -> gameController.game.state.toString()
-    )
-
-    //Ok(gameField)
-
-    /* wui needed
-    def setName(name: String) = Action {
-        controller.create_player(name)
-        if (!wui.getState().isInstanceOf[..]) {
-          if(wui.getState().isInstanceOf[..]) {
-            Redirect(" ")
-          } else {
-            Redirect(" ")
-          }
-        } else {
-          Ok( )
-        }
-      }
-     */
-  }*/
-
-    def init(): Action[AnyContent] = Action {
-        controller.initPlayers()
-        Redirect(routes.HomeController.field)
+    def rules(): Action[AnyContent] = Action {
+        Ok(views.html.rules())
     }
 
-    def initPlayers(): Action[AnyContent] = Action { request =>
-        val input = request.getQueryString("input").getOrElse("")
-        controller.addPlayer(input)
-        Redirect(routes.HomeController.field)
+    def playerNames(): Action[AnyContent] = Action {
+        Ok(views.html.playerNames())
+    }
+
+    def initPlayers(): Action[AnyContent] = Action { implicit request =>
+        val player1Name = request.body.asFormUrlEncoded.get("player1Name").mkString
+        val player2Name = request.body.asFormUrlEncoded.get("player2Name").mkString
+
+        controller.initPlayers()
+        controller.addPlayer(player1Name)
+        controller.addPlayer(player2Name)
+
+        Ok(views.html.playerPokemons(player1Name, player2Name))
     }
 
     def initPokemons(): Action[AnyContent] = Action { request =>
-        val input = request.getQueryString("input").getOrElse("")
-        controller.addPokemons(input)
-        Redirect(routes.HomeController.field)
+        val player1Pokemon = request.body.asFormUrlEncoded.get("player1Pokemon").mkString
+        val player2Pokemon = request.body.asFormUrlEncoded.get("player2Pokemon").mkString
+
+        controller.addPokemons(player1Pokemon)
+        controller.addPokemons(player2Pokemon)
+
+        Redirect(routes.HomeController.game)
     }
 
     def desicion(): Action[AnyContent] = Action { request =>
         val input = request.getQueryString("input").getOrElse("")
+
         controller.nextMove(input)
+
         Redirect(routes.HomeController.field)
     }
 
     def fighting(): Action[AnyContent] = Action { request =>
         val input = request.getQueryString("input").getOrElse("")
+
         controller.attackWith(input)
+
         Redirect(routes.HomeController.field)
     }
 
     def switch(): Action[AnyContent] = Action { request =>
         val input = request.getQueryString("input").getOrElse("")
+
         controller.selectPokemon(input)
+
         Redirect(routes.HomeController.field)
     }
 
     def restartGame(): Action[AnyContent] = Action {
         controller.restartTheGame()
+
         Redirect(routes.HomeController.field)
+    }
+
+    def field(): Action[AnyContent] = Action {
+        val gameString = controller.game.toString
+        val gameField = views.html.field(gameString, controller.game.state.toString, controller.game.turn)
+
+        Ok(gameField)
     }
 
     def game(): Action[AnyContent] = Action { request =>
         val command = request.getQueryString("command")
-        val input: String = command.getOrElse("");
-        if (input.charAt(0) == 'y') controller.undoMove()
-        else if (input.charAt(0) == 'z') controller.redoMove()
-        else
-            controller.game.state match {
-                case InitState() => controller.initPlayers()
-                case InitPlayerState() => controller.addPlayer( input )
-                case InitPlayerPokemonState() => controller.addPokemons( input )
-                case DesicionState() => controller.nextMove( input )
-                case FightingState() => controller.attackWith( input )
-                case SwitchPokemonState() => controller.selectPokemon( input )
-                case GameOverState() => controller.restartTheGame()
-            }
-        val gameString = controller.game.toString;
-        val gameField = views.html.game(gameString, controller.game.state.toString, controller.game.turn)
-        Ok(gameField)
-    }
+        val input: String = command.getOrElse("")
 
-    def field(): Action[AnyContent] = Action {
-        val gameString = controller.game.toString;
-        val gameField = views.html.field(gameString, controller.game.state.toString, controller.game.turn)
+        if(input.nonEmpty)
+            if (input.charAt(0) == 'y') controller.undoMove()
+            else if (input.charAt(0) == 'z') controller.redoMove()
+            else
+                controller.game.state match {
+                    case InitState() => controller.initPlayers()
+                    case InitPlayerState() => controller.addPlayer(input)
+                    case InitPlayerPokemonState() => controller.addPokemons(input)
+                    case DesicionState() => controller.nextMove(input)
+                    case FightingState() => controller.attackWith(input)
+                    case SwitchPokemonState() => controller.selectPokemon(input)
+                    case GameOverState() => controller.restartTheGame()
+                }
+
+        val gameString = controller.game.toString
+        val gameField = views.html.game(gameString, controller.game.state.toString, controller.game.turn)
+
         Ok(gameField)
     }
 
