77c7a9a7dd1fe9e80f1e8c686d91f9c4afb9137a
sxrxhmxsxrxs <sarahmisaras@icloud.com>
2023-10-22 23:19:43 +0200

    feat: added text to rules site, added title logo and pokeball start button

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -15,61 +15,10 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
 
     val controller: Controller = Controller()
 
-    def rules(): Action[AnyContent] = Action {
-        Ok(views.html.rules())
-    }
-
-    def playerNames(): Action[AnyContent] = Action {
-        Ok(views.html.playerNames())
-    }
-
-    def initPlayers(): Action[AnyContent] = Action { implicit request =>
-        val player1Name = request.body.asFormUrlEncoded.get("player1Name").mkString
-        val player2Name = request.body.asFormUrlEncoded.get("player2Name").mkString
-
-        controller.initPlayers()
-        controller.addPlayer(player1Name)
-        controller.addPlayer(player2Name)
-
-        Ok(views.html.playerPokemons(player1Name, player2Name))
-    }
-
-    def initPokemons(): Action[AnyContent] = Action { request =>
-        val player1Pokemon = request.body.asFormUrlEncoded.get("player1Pokemon").mkString
-        val player2Pokemon = request.body.asFormUrlEncoded.get("player2Pokemon").mkString
-
-        controller.addPokemons(player1Pokemon)
-        controller.addPokemons(player2Pokemon)
-
-        Redirect(routes.HomeController.game)
-    }
-
     def desicion(): Action[AnyContent] = Action { request =>
         val input = request.getQueryString("input").getOrElse("")
 
         controller.nextMove(input)
-
-        Redirect(routes.HomeController.field)
-    }
-
-    def fighting(): Action[AnyContent] = Action { request =>
-        val input = request.getQueryString("input").getOrElse("")
-
-        controller.attackWith(input)
-
-        Redirect(routes.HomeController.field)
-    }
-
-    def switch(): Action[AnyContent] = Action { request =>
-        val input = request.getQueryString("input").getOrElse("")
-
-        controller.selectPokemon(input)
-
-        Redirect(routes.HomeController.field)
-    }
-
-    def restartGame(): Action[AnyContent] = Action {
-        controller.restartTheGame()
 
         Redirect(routes.HomeController.field)
     }
@@ -81,11 +30,19 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
         Ok(gameField)
     }
 
-    def game(): Action[AnyContent] = Action { request =>
+    def fighting(): Action[AnyContent] = Action { request =>
+        val input = request.getQueryString("input").getOrElse("")
+
+        controller.attackWith(input)
+
+        Redirect(routes.HomeController.field)
+    }
+
+    def game(): Action[AnyContent] = Action { implicit request =>
         val command = request.getQueryString("command")
         val input: String = command.getOrElse("")
 
-        if(input.nonEmpty)
+        if (input.nonEmpty)
             if (input.charAt(0) == 'y') controller.undoMove()
             else if (input.charAt(0) == 'z') controller.redoMove()
             else
@@ -105,9 +62,55 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
         Ok(gameField)
     }
 
-
     def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
         Ok(views.html.index())
     }
 
+    def initPlayers(): Action[AnyContent] = Action { implicit request =>
+        val player1Name = request.body.asFormUrlEncoded.get("player1Name").mkString
+        val player2Name = request.body.asFormUrlEncoded.get("player2Name").mkString
+
+        controller.initPlayers()
+        controller.addPlayer(player1Name)
+        controller.addPlayer(player2Name)
+
+        Ok(views.html.playerPokemons(player1Name, player2Name))
+    }
+
+    def initPokemons(): Action[AnyContent] = Action { implicit request =>
+        val player1Pokemon1 = request.body.asFormUrlEncoded.get("player1Pokemon1").mkString
+        val player1Pokemon2 = request.body.asFormUrlEncoded.get("player1Pokemon2").mkString
+        val player1Pokemon3 = request.body.asFormUrlEncoded.get("player1Pokemon3").mkString
+        val player2Pokemon1 = request.body.asFormUrlEncoded.get("player2Pokemon1").mkString
+        val player2Pokemon2 = request.body.asFormUrlEncoded.get("player2Pokemon2").mkString
+        val player2Pokemon3 = request.body.asFormUrlEncoded.get("player2Pokemon3").mkString
+
+
+        controller.addPokemons(player1Pokemon1 + player1Pokemon2 + player1Pokemon3)
+        controller.addPokemons(player2Pokemon1 + player2Pokemon2 + player2Pokemon3)
+
+        Redirect(routes.HomeController.game)
+    }
+
+    def playerNames(): Action[AnyContent] = Action {
+        Ok(views.html.playerNames())
+    }
+
+    def restartGame(): Action[AnyContent] = Action {
+        controller.restartTheGame()
+
+        Redirect(routes.HomeController.field)
+    }
+
+    def rules(): Action[AnyContent] = Action {
+        Ok(views.html.rules())
+    }
+
+    def switch(): Action[AnyContent] = Action { request =>
+        val input = request.getQueryString("input").getOrElse("")
+
+        controller.selectPokemon(input)
+
+        Redirect(routes.HomeController.field)
+    }
 }
