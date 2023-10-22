7e5016fdd9d4fd4f592fbf68f8de85e1f76b3699
sxrxhmxsxrxs <sarahmisaras@icloud.com>
2023-10-22 18:02:06 +0200

    started working on html

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -17,6 +17,59 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
 
     val controller = new Controller();
 
+  def rules = Action {
+    Ok(views.html.rules())
+  }
+
+  def playerNames = Action {
+    Ok(views.html.playerNames())
+  }
+ //addplayers route could also be initplayersroute!
+  /*def addPlayers = Action { implicit request =>
+    val player1Name = request.body.asFormUrlEncoded.get("player1Name").mkString
+    val player2Name = request.body.asFormUrlEncoded.get("player2Name").mkString
+    val game = gameController.game
+
+    /*game.state.initPlayers()
+    println("0 state " + game.state)
+    game.state.addPlayer(player1Name)
+    game.state.addPlayer(player2Name)*/
+
+    gameController.initPlayers()
+    gameController.addPlayer(player1Name)
+    gameController.addPlayer(player2Name)
+
+
+    println("1 state " + gameController.game.state)
+
+
+    val gameString = gameController.game.toString;
+    val gameField = views.html.game(gameString)
+
+    Redirect(routes.HomeController.game).flashing(
+      "player1Name" -> player1Name,
+      "player2Name" -> player2Name,
+      "gameState" -> gameController.game.state.toString()
+    )
+
+    //Ok(gameField)
+
+    /* wui needed
+    def setName(name: String) = Action {
+        controller.create_player(name)
+        if (!wui.getState().isInstanceOf[..]) {
+          if(wui.getState().isInstanceOf[..]) {
+            Redirect(" ")
+          } else {
+            Redirect(" ")
+          }
+        } else {
+          Ok( )
+        }
+      }
+     */
+  }*/
+
     def init(): Action[AnyContent] = Action {
         controller.initPlayers()
         Redirect(routes.HomeController.field)
